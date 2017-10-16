e8cfe842ef6e111043f88fe71d74e3ef0edfd4ff
sandi <nitrixhost@gmail.com>
2017-10-16 13:26:46 +0700

    perubahan mainframe dan tambahan class

---
+++ README.md
@@ -0,0 +1,17 @@
+Copyright (c) 2017 Alfin Syamsuddin
+
+<h1>This middleware app for communicating with Twillio API SDK</h1>
+
+<h2>Requirements</h2>
+<ul>
+	<li>composer</li>
+	<li>php ^v5.3+</li>
+</ul>
+
+<h2>How to install this repo ?</h2>
+<ul>
+	<li>You need composer</li> 
+	<li>Install dependency package</li>
+	<li>run php -S localhost:8080 within your <i>ROOT DIRECTORY</i>/src/public </li>
+	<li>Enjoy</li> 
+</ul>
--- src/public/index.php
+++ src/public/index.php
@@ -10,20 +10,36 @@ require '../config/config.php';
 
 $app = new \Slim\App(["settings"=>$config]);
 
-$app->get('/hello/{name}', function (Request $request, Response $response){
-	$name = $request->getAttribute('name');
-	$response->getBody()->write("Hello, $name");
-
-	return $response;
-});
-
 $container = $app->getContainer();
-$container['logger'] = function($c) {
-	$logger = new \Monolog\Logger('my_logger');
+
+$container['logger'] = function($c){
+	$logger = new \Monolog\Logger('twil');
 	$file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
 	$logger->pushHandler($file_handler);
 	return $logger;
-}
+};
 
+/*$app->get('/v01/{name}', function(Request $request, Response $response){
+	$name = $request->getAttribute('name');
+	$response->getBody()->write("Hello, $name");
+	$this->logger->addInfo($response);
+	return $response;
+});*/
+
+$app->post('/v01/register', function(Request $request, Response $response){
+	$data = $request->getParsedBody();
+	
+	$tik_data = [];
+	$tik_data['title'] = filter_var($data['title'],FILTER_SANITIZE_STRING);
+	$tik_data['desc'] = filter_var($data['desc'],FILTER_SANITIZE_STRING);
+
+
+	if(empty($tik_data['title'])){
+		print_r("empty");
+	}else{
+		print_r("success");
+	}
+	$this->logger->addInfo(serialize($tik_data));
+});
 $app->run();
 ?>
